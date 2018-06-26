ee6e67248ccef1763e349e486565e18d252cc8e6
Will <williamjamesthompson@gmail.com>
2018-06-26 12:16:58 -0500

    Wow it's actually coming together

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+    //Hide questions until they are ready
+    $("#game2").hide();
+    $("#game3").hide();
+    $("#game4").hide();
 
 var userChoice;
 var wins = 0;
@@ -6,62 +10,126 @@ var losses= 0;
 
 var seinfeldQuestions = [{
     question: "The restaurant known as Monk's on the show is actually named what?",
-    choices: ["<button type='button' class='btn btn-outline-primary' id='ans'>Jerry's</button>" , "<button type='button' class='btn btn-outline-primary' id='ans'>Tom's</button>" , "<button type='button' class='btn btn-outline-primary' id='ans'>Harry's</button>" , "<button type='button' class='btn btn-outline-primary' id='ans'>Barry's</button>"],
-    correctAns: 1
+    choices: ["<input type='radio' name='option' value='jerrys' id='ans1'> Jerry's " , "<input type='radio' name='option' value='toms' id='ans2'> Tom's ", "<input type='radio' name='option' value='harrys'> Harry's " , "<input type='radio' name='option' value='barrys'> Barry's "],
+    correctAns: "toms"
 }, {
-    questions: "What are the names of George's horses at his fictitious home in the Hamptons?",
-    choices: ["<button type='button' class='btn btn-outline-primary' id='ans'>Thomas and Scamper</button>", "<button type='button' class='btn btn-outline-primary' id='ans'>Seabisquick and Billy the Kid</button>", "<button type='button' class='btn btn-outline-primary' id='ans'>Snoopy and Prickly Pete</button>", "<button type='button' class='btn btn-outline-primary' id='ans'>Runner and Jumper</button>"],
-    correctAns: 4
+    question: "What are the names of George's horses at his fictitious home in the Hamptons?",
+    choices: ["<input type='radio' name='option1' value='Thomas and Scamer' id='ans1'> Thomas and Scamper", "<input type='radio' name='option1' value='Seabisquick and Billy the Kid' id='ans1'> Seabisquick and Billy the Kid", "<input type='radio' name='option1' value='Snoopy and Prickly Pete' id='ans1'> Snoopy and Prickly Pete", "<input type='radio' name='option1' value='Runner & Jumper' id='ans1'> Runner & Jumper"],
+    correctAns: "Snoopy and Prickly Pete"
+}, {
+    question: "Other than Jerry, who uttered the words 'Hello Newman'?",
+    choices: ["<input type='radio' name='option2' value='Helen Seinfeld' id='ans1'> Helen Seinfeld", "<input type='radio' name='option2' value='George' id='ans1'> George", "<input type='radio' name='option2' value='Kramer' id='ans1'> Kramer", "<input type='radio' name='option2' value='Elaine' id='ans1'> Elaine"],
+    correctAns: "Helen Seinfeld"
+}, {
+    question: "What food was thrown at George at the end of the episode where the gang goes to the Hamptons?",
+    choices: ["<input type='radio' name='option3' value='Apple' id='ans1'> Apple", "<input type='radio' name='option3' value='Orange' id='ans1'> Orange", "<input type='radio' name='option3' value='Banana' id='ans1'> Banana", "<input type='radio' name='option3' value='Tomato' id='ans1'> Tomato"],
+    correctAns: "Tomato"
 }];
 
 
-//Question 1
+//Question 1 - Display on DOM
 $("#question1").text(seinfeldQuestions[0].question);
 
-//Buttons 1
+//Radio Buttons Question 1 - Display on DOM
 var choicesArr = seinfeldQuestions[0].choices;
 for (let i = 0; i < choicesArr.length; i++) {
     $("#ansDiv").append(choicesArr[i]);
 };
-$('#ansDiv').on('click', 'button', function(e){
-    seinfeldQuestions[0].correctAns;
-    userChoice = $(this).index();
 
-    if(userChoice != seinfeldQuestions[0].correctAns) {
-        losses++;
-        console.log("Wrong! " + losses);
-        
-    }
-    else {
-        wins++;
-        console.log("Correct! " + wins);
-    }
-});
+//Get the answer and move on to the next question
+$('#nextQ').click(function() {
+    $("#game1").hide();
+    $("#game2").show();
+    userChoice = ($('input[name=option]:checked', '#ansDiv').val());
+        if(userChoice != seinfeldQuestions[0].correctAns) {
+            losses++;
+            console.log("You are incorrect. " + losses);
+        } else {
+            wins++;
+            console.log("You are correct. " + wins);
+        }
+ });
 
-//Question 2
-$("#question1").text(seinfeldQuestions[0].question);
 
-//Buttons 2
+
+
+//Question 2 - Display on DOM
+$("#question2").text(seinfeldQuestions[1].question);
+
+//Radio Buttons Question 2 - Display on DOM
 choicesArr = seinfeldQuestions[1].choices;
 for (let i = 0; i < choicesArr.length; i++) {
     $("#ansDiv2").append(choicesArr[i]);
 };
-$('#ansDiv2').on('click', 'button', function(e){
-    userChoice = $(this).index();
-    console.log($(this).index());
-    if(userChoice != seinfeldQuestions[1].correctAns) {
-        losses++;
-        console.log("Wrong! " + losses)
-    }
-    else {
-        wins++;
-        console.log("Correct! " + wins);
-    }
-});
+
+//Get the answer and move on to the next question
+$('#nextQ1').click(function() {
+    $("#game2").hide();
+    $("#game3").show();
+    userChoice = ($('input[name=option1]:checked', '#ansDiv2').val());
+        if(userChoice != seinfeldQuestions[1].correctAns) {
+            losses++;
+            console.log("You are incorrect. " + losses);
+        } else {
+            wins++;
+            console.log("You are correct. " + wins);
+        }
+ });
 
 
 
 
 
+ //Question 3 - Display on DOM
+$("#question3").text(seinfeldQuestions[2].question);
 
+//Radio Buttons Question 3 - Display on DOM
+choicesArr = seinfeldQuestions[2].choices;
+for (let i = 0; i < choicesArr.length; i++) {
+    $("#ansDiv3").append(choicesArr[i]);
+};
+
+//Get the answer and move on to the next question
+$('#nextQ2').click(function() {
+    $("#game3").hide();
+    $("#game4").show();
+    userChoice = ($('input[name=option2]:checked', '#ansDiv3').val());
+        if(userChoice != seinfeldQuestions[2].correctAns) {
+            losses++;
+            console.log("You are incorrect. " + losses);
+        } else {
+            wins++;
+            console.log("You are correct. " + wins);
+        }
+ });
+
+
+
+
+  //Question 4 - Display on DOM
+$("#question4").text(seinfeldQuestions[3].question);
+
+//Radio Buttons Question 4 - Display on DOM
+choicesArr = seinfeldQuestions[3].choices;
+for (let i = 0; i < choicesArr.length; i++) {
+    $("#ansDiv4").append(choicesArr[i]);
+};
+
+//Get the answer and move on to the next question
+$('#nextQ3').click(function() {
+    $("#game4").hide();
+    $("#game5").show();
+    userChoice = ($('input[name=option3]:checked', '#ansDiv4').val());
+        if(userChoice != seinfeldQuestions[3].correctAns) {
+            losses++;
+            console.log("You are incorrect. " + losses);
+        } else {
+            wins++;
+            console.log("You are correct. " + wins);
+        }
+ });
+
+
+
+//This is the final endy
 });
